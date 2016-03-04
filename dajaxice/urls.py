58c077979e15363115eedc35f8e03e422deaa166
Felix Schlitter <felixSchlitter@gmail.com>
2016-03-05 06:33:43 +1300

    Do not try to import from django.conf.urls.default

--- dajaxice/urls.py
+++ dajaxice/urls.py
@@ -1,8 +1,4 @@
-try:
-    from django.conf.urls import patterns, url
-except ImportError:
-    from django.conf.urls.defaults import patterns, url
-
+from django.conf.urls import patterns, url
 from .views import DajaxiceRequest
 
 urlpatterns = patterns('dajaxice.views',
